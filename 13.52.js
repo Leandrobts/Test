5ca052c13f59ec9052008580db0540ddb5b777bd
Leandrobts <kazaalite_601@hotmail.com>
2026-08-12 21:30:51 -0300

    Add configuration for offsets in 13.52.js

---
+++ 13.52.js
@@ -0,0 +1,7 @@
+// 13.52 -- Configuração de Offsets (Slopkit Port)
+const OFFSET_wk_host_constructor_candidates = [
+    0x36F9E20   // Offset confirmado do __ps5NativeCtor
+];
+
+let wk_gadgetmap = {};
+let syscall_map = {};
